e8c3fd612a636ee7ad160e2da48365ed8fd65d48
Noah Moran <99003530+nmoran12@users.noreply.github.com>
2026-05-31 17:47:13 +0930

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> rightView;
+        
+        if (!root) return rightView;
+
+        // It needs to be a TreeNode*, not int, so you can access children.
+        queue<TreeNode*> myQueue;
+        myQueue.push(root);
+        int queueSize = 0;
+        
+
+
+        while (!myQueue.empty()){
+            queueSize = myQueue.size();
+
+            for (int i = 0; i < queueSize; i++){
+                // We need a local variable that points to the front of the queue, e.g. the current node we are on
+                // We need this curr node because 'root' ALWAYS points to the actual root of the tree, so we can't use root,
+                // we must make a new variable that points to the current node we are at.
+                TreeNode* curr = myQueue.front();
+
+                myQueue.pop();
+
+
+                if (i == (queueSize - 1)){
+                    rightView.push_back(curr->val);
+                }
+
+                if (curr->left != nullptr) myQueue.push(curr->left);
+                if (curr->right != nullptr) myQueue.push(curr->right);
+
+            }
+
+        }
+
+        return rightView;
+    }
+};

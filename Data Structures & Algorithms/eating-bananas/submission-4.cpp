103667b8d31d280c82138bb3f61a1d94ca1b95f1
Noah Moran <99003530+nmoran12@users.noreply.github.com>
2026-06-01 16:43:06 +0930

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int k = 0;
+        int onePointer = 1;
+        int twoPointer = piles.size() - 1;
+        int currentValue = 0;
+
+        for (int i = 0; i < piles.size(); i++){
+            twoPointer = max(twoPointer, piles[i]);
+        }
+
+        int answer = twoPointer;
+
+        while (onePointer <= twoPointer){
+            int mid = onePointer + (twoPointer - onePointer) / 2;
+            long long totalHours = 0;
+
+            for (int i = 0; i < piles.size(); i++){
+                totalHours += (piles[i] + mid - 1) / mid;
+            }
+
+
+            if (totalHours <= h){
+                answer = mid;
+                twoPointer = mid - 1;
+            } else {
+                onePointer = mid + 1;
+            }
+        }
+        
+        return answer;
+    }
+};

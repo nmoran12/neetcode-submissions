6873b0cf0642f78807116458d205b123285ab68e
Noah Moran <99003530+nmoran12@users.noreply.github.com>
2026-06-04 17:05:57 +0930

    Add: last-stone-weight - submission-5

---
+++ Data Structures & Algorithms/last-stone-weight/submission-5.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap;
+        int x = 0;
+        int y = 0;
+        int i = 0;
+        // left child = 2 * i + 1
+        // right child = 2 * i + 2
+
+        int k = 2;
+
+        // Add all the values to the max heap
+        for (int i = 0; i < stones.size(); i++){
+            maxHeap.push(stones[i]);
+        }
+
+        // We continue until there is no more than one stone remaining, hence size > 1
+        while (maxHeap.size() > 1){
+
+            // Put largest value into y, pop it
+            int y = maxHeap.top();
+            maxHeap.pop();
+
+            // Put new largest value into x (which is second largest overall since largest had been popped earlier), pop this
+            int x = maxHeap.top();
+            maxHeap.pop();
+
+            // We have already removed x and y from maxHeap, so dont have to remove again
+            // Just add y = y - x back if x != y
+            if (x < y){
+                maxHeap.push(y - x);
+            }
+        }
+
+        if (maxHeap.empty()){
+            return 0;
+        }
+
+
+        return maxHeap.top();
+    }
+};

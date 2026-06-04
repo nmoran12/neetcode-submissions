40ffb851e2562e8be3ce6cc9fbb92576c3cd086a
Noah Moran <99003530+nmoran12@users.noreply.github.com>
2026-06-04 17:03:59 +0930

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap;
+        int x = 0;
+        int y = 0;
+        int i = 0;
+        // left child = 2 * i + 1
+        // right child = 2 * i + 2
+
+        int k = 2;
+
+        for (int i = 0; i < stones.size(); i++){
+            maxHeap.push(stones[i]);
+        }
+
+        while (maxHeap.size() > 1){
+
+            int y = maxHeap.top();
+            maxHeap.pop();
+
+            int x = maxHeap.top();
+            maxHeap.pop();
+
+            if (x != y){
+                maxHeap.push(y - x);
+            }
+        }
+
+        if (maxHeap.empty()){
+            return 0;
+        }
+
+
+        return maxHeap.top();
+    }
+};

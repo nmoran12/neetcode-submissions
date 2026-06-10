bbff7cf2d697130789f1aa11e8764c4027669229
Noah Moran <99003530+nmoran12@users.noreply.github.com>
2026-06-10 18:22:16 +0930

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReachable = 0;
+
+        for (int i = 0; i < nums.size(); i++){
+
+            if (i > maxReachable){
+                return false;
+            }
+
+            maxReachable = max(maxReachable, i + nums[i]);
+
+            if (maxReachable >= nums.size() - 1){
+                return true;
+            }
+        }
+        
+
+        return true;
+    }
+};

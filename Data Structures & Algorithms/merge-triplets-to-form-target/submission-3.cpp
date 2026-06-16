e13991f66d48cca17155bbaaa7e2c90c92898338
Noah Moran <99003530+nmoran12@users.noreply.github.com>
2026-06-16 15:12:59 +0930

    Add: merge-triplets-to-form-target - submission-3

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        bool results1 = false;
+        bool results2 = false;
+        bool results3 = false;
+
+
+        for (int i = 0; i < triplets.size(); i++){
+
+            if (triplets[i][0] > target[0] || triplets[i][1] > target[1] || triplets[i][2] > target[2]){
+                continue;
+            }
+
+            if (triplets[i][0] == target[0]){
+                results1 = true;
+            }
+            
+            if (triplets[i][1] == target[1]){
+                results2 = true;
+            }
+            
+            if (triplets[i][2] == target[2]){
+                results3 = true;
+            }
+
+        }
+
+        return results1 && results2 && results3;
+    }
+};

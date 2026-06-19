00e86768ac8380750733191a10495b0600312c2e
Noah Moran <99003530+nmoran12@users.noreply.github.com>
2026-06-19 17:09:53 +0930

    Add: longest-substring-without-duplicates - submission-21

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-21.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int left = 0;
+        int maxLength = 0;
+
+        unordered_map<char, int> freqMap;
+
+        for (int right = 0; right < s.length(); right++){
+            freqMap[s[right]]++;
+
+            while (freqMap[s[right]] > 1){
+                freqMap[s[left]]--;
+                left++;
+            }
+
+
+            maxLength = max(maxLength, right - left + 1);
+        }
+        
+
+        return maxLength;
+    }
+};

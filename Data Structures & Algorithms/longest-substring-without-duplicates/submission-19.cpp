f43cd2b2917b3226a46b95ee8a6afd7b5418a300
Noah Moran <99003530+nmoran12@users.noreply.github.com>
2026-06-19 17:09:12 +0930

    Add: longest-substring-without-duplicates - submission-19

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-19.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int left = 0;
+        int maxLength = 0;
+
+        unordered_map<char, int> freqMap;
+
+        for (int right = 0; right < s.length(); right++) {
+            // FIX: Added the ++ to correctly increment the frequency count
+            freqMap[s[right]]++; 
+
+            while (freqMap[s[right]] > 1) {
+                freqMap[s[left]]--;
+                left++;
+            }
+
+            maxLength = max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+};

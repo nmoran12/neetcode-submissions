16a9a5e30304e6b55d21fee0d36ff94cac3b092a
Noah Moran <99003530+nmoran12@users.noreply.github.com>
2026-06-23 16:23:41 +0930

    Add: kth-largest-element-in-an-array - submission-9

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-9.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+
+        std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+
+        for (int values : nums){
+            minHeap.push(values);
+
+            if (minHeap.size() > k){
+                minHeap.pop();
+            }
+        }
+        
+        return minHeap.top();
+    }
+};

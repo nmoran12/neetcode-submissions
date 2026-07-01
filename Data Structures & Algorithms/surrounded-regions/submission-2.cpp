bb679ac070085258fac726431669ef99e5c31823
Noah Moran <99003530+nmoran12@users.noreply.github.com>
2026-07-01 14:05:04 +0930

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+
+    void capture(vector<vector<char>>& board, int r, int c){
+        // Base Case 1: If we go out of bounds or if current cell is NOT an 'O'
+        if (r < 0 || r >= board.size() || c < 0 || c >= board[0].size() || board[r][c] != 'O'){
+            return;
+        }
+
+        // Mark
+        board[r][c] = 'T';
+
+        capture(board, r, c - 1); // left
+        capture(board, r, c + 1); // right
+        capture(board, r - 1, c); // up
+        capture(board, r + 1, c); // down
+
+    }
+
+
+    void solve(vector<vector<char>>& board) {
+        int rows = board.size();
+        int cols = board[0].size();
+
+        // 1. (DFS) Capture unsurrounded regions (O -> T)
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < cols; c++){
+                // Check if we are on the borders
+                if (board[r][c] == 'O' && ((r == 0 || r == rows - 1) || c == 0 || c == cols - 1)){
+                    capture(board, r, c);
+                }
+
+            }
+        }
+
+
+
+
+        // 2. Capture surrounded regions (O -> X)
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < cols; c++){
+                if (board[r][c] == 'O'){
+                    board[r][c] = 'X';
+                }
+            }
+        }
+
+
+        // 3. Change all T's back to O's
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < cols; c++){
+                if (board[r][c] == 'T'){
+                    board[r][c] = 'O';
+                }
+            }
+        }
+    }
+};

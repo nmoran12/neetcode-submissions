78fa2beb36cdaeab7d68bae3c7f66aeeeb743c9f
Noah Moran <99003530+nmoran12@users.noreply.github.com>
2026-07-07 15:38:10 +0930

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+
+    void dfs(int node, vector<vector<int>>& adjacencyList, vector<bool>& visited){
+        // base case: if node is visited, just return
+        if (visited[node]){
+            return;
+        }
+
+
+        // Mark as visited
+        visited[node] = true;
+
+
+        // Process it
+        for (int neighbours : adjacencyList[node]){ // Look at every node connected to my current node
+            dfs(neighbours, adjacencyList, visited); // Go explore that neighbour
+        }
+
+
+    }
+
+    int countComponents(int n, vector<vector<int>>& edges) {
+        
+        vector<vector<int>> adjacencyList(n);
+        vector<bool> visited(n, false);
+        int componentCount = 0;
+        
+        // Populate the undirected graph adjacency list
+        for (auto edge : edges){
+
+            int a = edge[0];
+            int b = edge[1];
+
+            adjacencyList[a].push_back(b);
+            adjacencyList[b].push_back(a);
+        }
+
+        for (int i = 0; i < n; i++){
+
+                if (!visited[i]){
+                    componentCount++;
+                    dfs(i, adjacencyList, visited);
+                }
+        }
+
+        return componentCount;
+    }
+};

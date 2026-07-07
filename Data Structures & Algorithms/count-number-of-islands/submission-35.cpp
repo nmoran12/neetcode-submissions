c2cad47a8de78d2a4c655cbc495d281b1c59f140
Noah Moran <99003530+nmoran12@users.noreply.github.com>
2026-07-07 10:26:46 +0930

    Add: count-number-of-islands - submission-35

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-35.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+
+        queue<pair<int, int>> myQueue;
+        int islandCount = 0;
+
+        vector<pair<int, int>> directions({{-1, 0}, {1, 0}, {0, -1}, {0, 1}});
+
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < cols; c++){
+
+                if (grid[r][c] == '1' && !visited[r][c]){
+                    
+                    visited[r][c] = true;
+                    myQueue.push({r, c});
+                    islandCount++;
+
+                    while (!myQueue.empty()){
+
+                        // MUST GET CURRROW, CURRCOL HERE
+                        auto [currRow, currCol] = myQueue.front();
+                        myQueue.pop();
+
+                        for (auto dir : directions){
+                            int newRow = currRow + dir.first;
+                            int newCol = currCol + dir.second;
+
+                            if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && !visited[newRow][newCol] && grid[newRow][newCol] == '1'){
+                                visited[newRow][newCol] = true;
+                                myQueue.push({newRow, newCol});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return islandCount;
+    }
+};

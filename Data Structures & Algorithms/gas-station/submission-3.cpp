ba2ad8f0b0717dde9f954fb1f18e313ce0defb8e
Noah Moran <99003530+nmoran12@users.noreply.github.com>
2026-07-22 14:32:19 +0930

    Add: gas-station - submission-3

---
+++ Data Structures & Algorithms/gas-station/submission-3.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int sumGas = 0;
+        int sumCost = 0;
+
+        for (int i = 0; i < gas.size(); i++){
+            sumGas += gas[i];
+            sumCost+= cost[i];
+        }
+
+        // if total gas is less than total cost, we can't do it, return -1
+        if (sumGas < sumCost){
+            return -1;
+        }
+
+        int totalSum = 0;
+        int index = 0;
+        int startIndex = 0;
+
+        for (int i = 0; i < gas.size(); i++){
+            totalSum += (gas[i] - cost[i]);
+
+            if (totalSum < 0){
+                totalSum = 0;
+                // move start index forward by one
+                startIndex = i + 1;
+            }
+        }
+        
+
+        return startIndex;
+    }
+};

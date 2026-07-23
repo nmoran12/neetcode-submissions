7c862bc407718c3ebf8cb09abca01f630756bb72
Noah Moran <99003530+nmoran12@users.noreply.github.com>
2026-07-23 14:20:47 +0930

    Add: partition-labels - submission-5

---
+++ Data Structures & Algorithms/partition-labels/submission-5.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+
+        vector<int> hashMap(26);
+
+        vector<int> result;
+        int sizeOfPartition = 0;
+        int endOfPartition = 0;
+
+        for (int i = 0; i < s.length(); i++){
+            hashMap[s[i] - 'a'] = i;
+        }
+
+        for (int i = 0; i < s.length(); i++){
+            sizeOfPartition++;
+
+            endOfPartition = max(endOfPartition, hashMap[s[i] - 'a']);
+
+            if (i == endOfPartition){
+                result.push_back(sizeOfPartition);
+                sizeOfPartition = 0;
+
+            }
+
+
+
+        }
+        
+        return result;
+    }
+};

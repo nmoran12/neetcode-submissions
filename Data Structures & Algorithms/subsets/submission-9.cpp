80ed5f6e07666139d3e92460fa1cffe363c772f5
Noah Moran <99003530+nmoran12@users.noreply.github.com>
2026-07-24 16:19:14 +0930

    Add: subsets - submission-9

---
+++ Data Structures & Algorithms/subsets/submission-9.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    vector<vector<int>> results;
+    vector<int> subset;
+
+    void dfs(vector<int>& nums, int i){
+        if (i == nums.size()){
+            results.push_back(subset);
+            return;
+        }
+
+        // Decide to include nums[i]
+        subset.push_back(nums[i]);
+        dfs(nums, i + 1); // Going down the branch to include all subsets that have [1] at the start e.g. [1, 2, 3], [1, 2], [1, 3], etc.
+
+        // Decide to exclude nums[i]
+        subset.pop_back();
+        dfs(nums, i + 1);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int i = 0;
+
+        dfs(nums, i);
+
+        return results;
+    }
+};
